253dabea64808efa5867e6d137a35af02398c8eb
David Hsieh <dcchsieh@gmail.com>
2018-01-11 22:19:43 +1000

    Added method to
    WIP move the 'DownloadingService' as an interactor using mediator pattern and separate that out with the gateway implementation to IgMarkets API

---
+++ src/IgMarketsDataDownloader/Configuration/AppSettings.cs
@@ -0,0 +1,34 @@
+﻿namespace IgMarketsDataDownloader.Configuration
+{
+    public class AppSettings
+    {
+        public Profile Profile { get; set; }
+        
+        public RestEndpoint RestEndpoint { get; set; }
+    }
+
+    public class Profile
+    {
+        public string Environment { get; set; }
+
+        public string DemoBaseUrl { get; set; }
+
+        public string LiveBaseUrl { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string ApiKey { get; set; }
+    }
+
+    public class RestEndpoint
+    {
+        public Login Login { get; set; }
+    }
+
+    public class Login
+    {
+        public string Session { get; set; }
+    }
+}
---
+++ src/IgMarketsDataDownloader/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace IgMarketsDataDownloader
+{
+    public static class Constants
+    {
+        public static string IgApiKeyHeader = "X-IG-API-KEY";
+
+        public static string IgMarketsUserNameConfigKey = "IgMarketsUserName";
+
+        public static string IgMarketsPasswordConfigKey = "IgMarketsPassword";
+
+        public static string IgMarketsApiKeyConfigKey = "IgMarketsApiKey";
+    }
+}
